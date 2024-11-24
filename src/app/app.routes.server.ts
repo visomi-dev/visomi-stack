69d82f06b20e2ee1dc8898680acf058100e933a3
Michael Villalba Sotelo <visomi.dev@gmail.com>
2024-11-24 00:21:01 -0600

    fix: trying to implement api server inside app server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'sign-in', renderMode: RenderMode.Prerender },
+  { path: 'dashboard', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,17 @@ import {
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
+import morgan from 'morgan';
+
+import api from './api/main';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
+
+app.use(morgan('dev'));
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -30,13 +35,7 @@ const angularApp = new AngularNodeAppEngine();
 /**
  * Serve static files from /browser
  */
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
+app.use(express.static(browserDistFolder));
 
 /**
  * Handle all other requests by rendering the Angular application.
@@ -55,6 +54,8 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
+  await api(app);
+
   const port = process.env['PORT'] ?? 4200;
 
   app.listen(port, () => {
